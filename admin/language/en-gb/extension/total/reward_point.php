ce3186fd1de411ee4746421fc0e58d476a6878c9
Ankit Chaudhary <ankit@akratech.com>
2021-01-28 20:12:48 +0530

    Reward file

---
+++ admin/language/en-gb/extension/total/reward_point.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'Reward Point Redemption';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified reward point redemption total!';
+$_['text_edit']        = 'Edit reward point redemption Total';
+
+// Entry
+$_['entry_total']      = 'Order Total';
+$_['entry_fee']        = 'Fee';
+$_['entry_tax_class']  = 'Tax Class';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Help
+$_['help_total']       = 'The checkout total the order must reach before this order total becomes active.';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify reward point redemption total!';
